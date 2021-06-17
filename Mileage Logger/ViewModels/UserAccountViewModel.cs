44ccb6f873ec419d8c3ec991d74378ea68948f17
DrOellermann <u19057416@tuks.co.za>
2021-06-17 10:51:57 +0200

    Added view models

---
+++ Mileage Logger/ViewModels/UserAccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mileage_Logger.Models;
+
+namespace Mileage_Logger.ViewModels
+{
+    public class UserAccountViewModel
+    {
+        private UserAccount UserAccount { get; set; }
+    }
+}
