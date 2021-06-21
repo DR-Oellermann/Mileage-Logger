ac4af1f3da809130097745a1a8bf3209320f9380
DrOellermann <u19057416@tuks.co.za>
2021-06-21 11:50:13 +0200

    Added username to navbar after login

--- Mileage Logger/Controllers/HomeController.cs
+++ Mileage Logger/Controllers/HomeController.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mileage_Logger.IdentityManagement;
 
 namespace Mileage_Logger.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Auth(Roles = ("admin"))]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        [AuthAttribute(Roles = ("admin"))]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
--- Mileage Logger/IdentityManagement/IdentityPrincipal.cs
+++ Mileage Logger/IdentityManagement/IdentityPrincipal.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Principal;
 using System.Web;
+using System.Web.WebPages;
+using Microsoft.AspNet.Identity;
 using Mileage_Logger.Models;
+using Newtonsoft.Json.Serialization;
 
 namespace Mileage_Logger.IdentityManagement
 {
     public class IdentityPrincipal
     {
         private UserAccount _userAccount;
+        milageTrackerEntities db = new milageTrackerEntities();
+        private tblUser TblUser = new tblUser();
 
         public IIdentity Identity { get; set; }
 
@@ -23,7 +28,7 @@ namespace Mileage_Logger.IdentityManagement
         {
             //need to fix to take user and admin -- done splits with comma (use comma in mdoel when authorize attribute)
             var roles = role.Split(new char[] { ',' });
-            return roles.Any(x => this._userAccount.Role.Contains(x));
+            return roles.Any(x => this.TblUser.IsAdmin.Contains(x));
         }
 
     }
