4bc0b39224c79675ed697e9fcbd5c8d471433037
DrOellermann <u19057416@tuks.co.za>
2021-06-21 10:01:54 +0200

    simplified login

--- Mileage Logger/Models/AccountModel.cs
+++ Mileage Logger/Models/AccountModel.cs
@@ -10,38 +10,22 @@ namespace Mileage_Logger.Models
     public class AccountModel
     {
         milageTrackerEntities db = new milageTrackerEntities();
-        private List<UserAccount> listAccounts = new List<UserAccount>();
-
-        public AccountModel()
-        {
-            //listAccounts.Add(new UserAccount {Username = "admin", Password = "admin", Role = "admin"});
-
-            //add all users and passwords to lsit
-            //uses email as username now not first name
-            foreach (var item in db.tblUsers)
-            {
-                listAccounts.Add(new UserAccount { UserID = item.User_ID, Username = item.Email, Password = item.Password, Role = item.IsAdmin });
-            }
-
-        }
 
         //finds the suers with specific username
-        public UserAccount findUser(string username)
+        public tblUser findUser(string username)
         {
-            return listAccounts.FirstOrDefault(x => x.Username == username);
+            return db.tblUsers.FirstOrDefault(x => x.Username == username);
         }
 
         // returns the ID of the user
         public int findUserId(string username)
         {
-            return listAccounts.Where(x => x.Username == username).Select(x => x.UserID).FirstOrDefault();
+            return db.tblUsers.Where(x => x.Username == username).Select(x => x.User_ID).FirstOrDefault();
         }
 
         //use for login to make to username and passwd match whats in db/list
         public tblUser Login(string username, string password)
         {
-            Debug.WriteLine(username + "     " + password);
-            Debug.WriteLine(db.tblUsers.FirstOrDefault(x => x.Username == username && x.Password == password));
             return db.tblUsers.FirstOrDefault(x => x.Username == username && x.Password == password);
         }
 
