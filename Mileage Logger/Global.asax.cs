08b139b778a1cadc44cdf7a8f87c5156ddc42cfd
DrOellermann <u19057416@tuks.co.za>
2021-07-06 11:49:33 +0200

    added webapi package

--- Mileage Logger/Global.asax.cs
+++ Mileage Logger/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace Mileage_Logger
 {
@@ -12,6 +13,7 @@ namespace Mileage_Logger
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
